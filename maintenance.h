2dfe6725dfe6baeb0a99a16c578cdc8e71c66061
Karenmajala <116045128+Karenmajala@users.noreply.github.com>
2023-04-12 23:00:29 -0500

    Create maintenance.h

---
+++ maintenance.h
@@ -0,0 +1,12 @@
+#ifndef maintenance_h
+#define maintenance_h
+#include <iostream>
+void initializeArrays(int *array, int size)
+{
+    for(int i = 0; i < size; i++)
+    {
+        array[i] = 0;
+    }
+}
+
+#endif /* maintenance_h */
