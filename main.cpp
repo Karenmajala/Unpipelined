b12f1c2cf20cc20ae5d514a6df26b06a716e267a
Karenmajala <116045128+Karenmajala@users.noreply.github.com>
2023-04-14 18:26:38 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,138 +2,69 @@
 #include <fstream>
 #include <sstream>
 #include <string>
-#include <stdlib.h>
-#include <cmath>
+//#include <stdlib.h>
+#include <vector>
 #include "maintenance.h"
+#include "datapath.h"
 using namespace std;
-int binaryToDecimal(string binary)
-{
-    int decimal = 0;
-    int power = 0;
-    for (int i=(binary.size()-1) ; i>=0; i--)
-    {
-        if (binary[i] == '1')
-        {
-            decimal += pow(2, power);
-        }
-        
-        power++;
-    }
-    return decimal;
-};
-struct instruction{
-  char type;
-  int op;
-  int rs;
-  int rt;
-  int rd; 
-  int shamt;
-  int function;
-  int immed;
-};
-int main()
-{
-    
-    ifstream fileInput;
-    ofstream fileOutput;
-    fileInput.open("input.txt");
+int step, inum, PC;
+int main(){
+  
+//file loading
+  string inputFile, outputFile;
+  char run='y';
+  ifstream fileInput;
+  ofstream fileOutput;
+  
+  while(run=='y'){
+  cout<< "Enter the input and output file names separated by a space: "<<endl;
+  cin>>inputFile>>outputFile;
+  fileInput.open(inputFile);
     if(!fileInput.is_open()){
 		  cout << "Could not open file ";
-		  return -1;
-    }
-    fileOutput.open("Output.txt");
+		  return -1;}
+  fileOutput.open(outputFile);
     
-    
-    string line;
-    istringstream numbers(line);
-    int *registers = new int[32];
-    int *memory = new int[1000];
-    initializeArrays(registers, 32);
-    initializeArrays(memory, 1000);
-    int regNum, memNum, regVal, memVal;
-
-  //main code
-    fileInput>>line;
-    if(line=="REGISTERS"){
-      fileInput>>line;
-      int R;
-      string subR ;
-      do
-      {
-        //Rs will always come in pairs
-        subR = line.substr(1);
-        R= stoi(subR);
-        fileInput>>line;
-        registers[R] = stoi(line);
-        fileInput>>line;
-      }while(line!="MEMORY");
-      
-    }else{ cout<< "File not in correct Format"<<endl;
-         return -1;}
-  //MEMORY
-  int Mem;
-  fileInput>>line;
-  do
-      {
-        Mem = stoi(line);
-        fileInput>>line;
-        memory[Mem] = stoi(line);
-        fileInput>>line;
-      }while(line!="CODE");
-  //code
-  fileInput>>line;
-  //cout<<instruction fetch
-  //decode
+//initialize
+  step=1;
+  inum=1; 
+  PC=0;
+  vector<string> code;
+  MultiCycleReg mc;
   instruction inst;
-  string subC;
-  subC = line.substr(0,6);
-  inst.op = stoi(subC);
-  subC=line.substr(6,5); //getting rs
-  inst.rs = binaryToDecimal(subC);
-  subC=line.substr(11,5); //getting rt
-  inst.rt = binaryToDecimal(subC);
-  subC=line.substr(16,5); //getting rd
-  inst.rd = binaryToDecimal(subC);
-  if(inst.op==0){ 
-    inst.type = 'R';
-    subC=line.substr(21,5);
-    inst.shamt = binaryToDecimal(subC);
-    subC=line.substr(26,6);
-    inst.function = stoi(subC);
-  }else{inst.type = 'I';
-    subC=line.substr(16,16);
-    inst.immed = binaryToDecimal(subC);}
-  //cout<<instruction decode
-  //arithmetic part
+  int *registers = new int[32];
+  int *memory = new int[250]; //int is always 32 bits
+  initializeArrays(registers, 32);
+  initializeArrays(memory, 250);
+  loadregisters(fileInput, registers);
+  loadmemory(fileInput, memory);
   
-  if(inst.type=='R'){
-    if(inst.function == 100000){//ADD
-      
-    }
-    else if(inst.function == 100010){//SUB
-      
-    }
-    else if(inst.function == 101010){//SLT
-      
-    }
-  }else{
-    if(inst.op == 001000){//ADDI
-    }
-    else if(inst.op == 100011){//LW
-    }
-    else if(inst.op == 101011){//SW
-    }
-    else if(inst.op == 000100){//BEQ
-    }
-    else if(inst.op == 000101){//BNE
-    }
+  //load in code so that it can be indexed
+  while(!fileInput.eof()){
+   string line;
+   fileInput>>line;
+   code.push_back(line);
   }
   
-    
-    
-    
-    fileInput.close();
-    fileOutput.close();
-    delete [] registers;
-    delete [] memory;
-}
+  //cycle steps
+  while(PC<code.size()){
+   IF(fileOutput, mc, code); 
+   ID(fileOutput, mc, inst,registers);
+    //the following steps are dependent on instruction classification
+   if(inst.type=='R'){ ClassR(fileOutput, mc, inst, registers);}
+   else{ ClassI(fileOutput, mc, inst, registers, memory);}
+   inum++; 
+  }
+
+  printRegisters(fileOutput, registers);
+  printMemory(fileOutput, memory);
+  fileInput.close();
+  fileOutput.close();
+  delete [] registers;
+  delete [] memory;
+  code.clear();
+
+  cout<<"Would you like to simulate another round? (y/n)"<<endl;
+  cin>>run;
+  
+}}
